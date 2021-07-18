ea719cca614e89af075dacdc50dfef88fed9b294
Rui Santos <elrui@users.noreply.github.com>
2021-07-17 21:49:16 -0500

    Add files via upload

---
+++ olimp.py
@@ -0,0 +1,117 @@
+'''
+Comentarios:
+** La tarea requiere que las cosas se guarden en un archivo, el programa al arrancar deberia leer el archivo y actualizar su contenido
+** También indica que se deben usar funciones. Supongo que se refiere a las funciones que definimos ayer
+
+Para la pregunta 1, estoy asumiendo que se puede:
+  - Agregar un atleta nuevo
+  - Agregar medallas a un atleta existente
+  - Si un atleta puede participar en más de un deporte debe tener ID distinto (para que los ID sean unicos)
+  - El ID se introduce a mano y puede ser alfanumerico. Si se quiere que sea autoincremental hay que hacer una función extra para eso
+
+Para la pregunta 2, simplemente se consulta la base de datos y se formatea como tabla
+
+
+
+'''
+##Algoritmo Medallero
+
+import os
+import platform
+
+import olimpiclib as ol 
+
+filename='medallero.csv'
+
+# Verificar si el archivo existe, y de no ser así, crearlo vacío
+if not os.path.isfile(filename):
+    ol.createFile(filename)
+
+
+cls = 'cls' if platform.system() == 'Windows' else 'clear'   ## Esto es solo para que el cls funcione en todas las plataformas, se puede omitir
+
+def menu():  
+    os.system(cls)
+    print("Medallero Olimpico".center(60))
+    print("\n\n")
+    print("1.  Registro de Atletas")
+    print("2.  Consultar medallas obtenidas por pais")
+    print("3.  Consultar pais con mayor numero de medallas")
+    print("4.  Consultar medallas por atleta")
+    print("5.  Listar por un deporte X")
+    print("6.  Salir")
+
+    opcion = input("Ingrese la Opcion del menu: ")
+    return opcion
+
+#Se invoca la función
+while True:
+    registry = ol.readFile(filename)    # leer el archivo y actualizar el registro cada vez que se carga el menu
+    opcion = menu()
+    
+    ##1 Registrar atleta
+
+    if opcion == '1':
+
+        while True:
+            id      = int(input('Ingrese el ID del atleta: '))
+
+            atleta = ol.findAthlete(id, registry)
+
+            if not atleta:      # si el atleta con ese ID no existe, se crea
+                nombre  = input('Ingrese el nombre del atleta: ')
+                pais    = input('Ingrese el país al que representa el atleta: ')
+                deporte = input('Indique en qué deporte compite el atleta: ')
+                registry = ol.createAthlete(id, nombre, pais, deporte, registry)
+                
+            else: 
+                print('Nombre del atleta: ', atleta['name'])
+                print('País al que representa: ', atleta['country'])
+                print('Deporte en el que compite: ', atleta['sport'])
+
+            medallas = int(input('Indique número de medallas a cargar (0 para salir) '))
+
+            while medallas > 0:
+                tipo = input('Indique tipo de medalla a cargar (Oro: 1, Plata: 2, Bronce: 3) ')
+                if tipo == '1':
+                    registry = ol.addMedal(id, 'gold', 1, registry)
+                elif tipo == '2':
+                    registry = ol.addMedal(id, 'silver', 1, registry)
+                elif tipo == '3':
+                    registry = ol.addMedal(id, 'bronze', 1, registry)
+                else:
+                    pass
+
+                medallas -= 1
+
+            otro = input('Desea agregar otro atleta? (S/N) ').lower()
+
+            if otro != 's':
+                break    
+
+        ol.writeFile(filename, registry)    # Actualizar el archivo en disco
+
+
+    ##2 Medallas por país
+
+    elif opcion =='2':
+        os.system(cls)
+        country = input("Ingrese el nombre del pais a consultar: ")
+        
+        atletas = ol.byCountry(country, registry)
+        
+        print('{:<30} {:<30} {:<8} {:<8} {:<8} '.format('Nombre', 'Deporte', 'Oro', 'Plata', 'Bronce')) 
+
+        for atleta in atletas:
+            print('{:<30} {:<30} {:<8d} {:<8d} {:<8d} '.format(atleta['name'], atleta['sport'], int(atleta['gold']), int(atleta['silver']), int(atleta['bronze']))) 
+        
+        while True:
+            _ = input('Presione enter para volver al menú principal ')
+            break
+
+    ##6
+
+    elif opcion =='6':
+        os.system(cls)
+        print("Adios".center(60))
+        break
---
+++ olimpiclib.py
@@ -0,0 +1,135 @@
+''''
+Librería de funciones para manejo de medalleros olímpicos
+'''
+
+import csv
+
+FIELDS = ['id', 'name', 'country', 'sport', 'gold', 'silver', 'bronze']
+
+## Sample record:  ('111', 'Jhon Goodman', 'US', 'Running', '1', '3', '0')
+## Title/Header: (id, name, country, sport, gold, silver, bronze)
+
+## Create a dictionary {id: Athlete}
+
+def readFile(filename, fields=FIELDS):
+    '''
+    This function takes a filename and returns a list of records from the file
+    The records are of type dictionary
+    '''
+
+    athletes = [] 
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)     # don't pass fields if file has headers
+    
+        for row in reader:
+            athletes.append(row)
+
+    return athletes
+            
+def writeFile(filename, athletes, fields=FIELDS):
+    '''
+    This function takes a filename and a list of records, and save the records in said file
+    Fields must be specified in order to write headers
+    The records are of type dictionary
+    '''
+
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+        writer.writeheader()
+        for row in athletes:
+            writer.writerow(row)
+
+def createFile(filename, fields=FIELDS):
+    '''
+    Initialize a file with headers
+    '''
+
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+        writer.writeheader()
+
+def findAthlete(id, registry):
+
+    for person in registry:
+        if person['id'] == str(id):
+            return person
+    
+    return False
+
+def createAthlete(id, name, country, sport, registry):
+    '''
+    Answers the question 
+    1. Registro de atleta
+    '''
+
+    if findAthlete(id, registry):
+        return False      ## Athlete already exists, so function fails and returns False
+    else:
+        newath = {}
+        newath['id']      = str(id)
+        newath['name']    = name
+        newath['country'] = country
+        newath['sport']   = sport
+        newath['gold']    = '0'
+        newath['silver']  = '0'
+        newath['bronze']  = '0'
+
+        registry.append(newath)
+        return registry
+
+
+def byCountry(country, registry):
+    '''
+    Answers the question
+    2. Consulta de Medallas obtenidas por un país X. 
+    (Nombre del Atleta, Deporte y Cantidad de Medallas).
+    '''
+
+    result = []
+
+    for person in registry:
+        if person['country'] == country:
+            result.append(person)
+    
+    return result
+
+def bySport(sport, registry):
+    '''
+    Finds all athletes and medals by sport
+    '''
+
+    result = []
+
+    for person in registry:
+        if person['sport'] == sport:
+            result.append(person)
+    
+    return result
+
+def addMedal(id, type, qty, registry):
+    '''
+    Add medal to a given athlete
+    '''
+    athlete = findAthlete(id, registry)
+
+    if not athlete:
+        return registry
+    else: 
+        athlete[type] = str(int(athlete[type]) + qty)   ### this works because lists support changes in place
+        return registry
+
+
+def totalByCountry(country, registry):
+    gold = silver = bronze = 0
+
+    for person in registry:
+        if person['country'] == country:
+            gold += int(person['gold'])
+            silver += int(person['silver'])
+            bronze += int(person['bronze'])
+    
+    return (gold, silver, bronze)
+            
+
